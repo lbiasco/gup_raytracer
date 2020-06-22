d7dc1bb0ae2c2a832c46c22d6ffe0b5cb962676a
Levi Biasco <levi.biasco@gmail.com>
2020-06-22 00:54:49 -0700

    Ex 3.4 - Change vp pixel size and resolution

--- src/build_funcs/build_single_sphere.cpp
+++ src/build_funcs/build_single_sphere.cpp
@@ -1,13 +1,13 @@
 void 												
 World::Build(void) {
-    vp_.SetHRes(200);
-	vp_.SetVRes(200);
-	vp_.SetPixelSize(1.0);
+    vp_.SetHRes(100);
+	vp_.SetVRes(100);
+	vp_.SetPixelSize(3);
 	vp_.SetGamma(1.0);
 	
 	background_color_ = kWhite;
 	tracer_ptr_ = new SingleSphere(this); 
 	
 	sphere_.SetCenter(Point3D(50.0, 0.0, 0.0));
-	sphere_.SetRadius(150);
+	sphere_.SetRadius(100);
 }
